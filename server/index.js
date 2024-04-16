a04098647f0bf9b2a9e8d7d2428cf0ea28f2ffb1
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-04-16 22:53:30 +0200

    fixed cors so i can fetch data from server, and tested to fetch the products!  ITS WORKING! HAPPY BOI :D

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
 
 //connecting to my server.
 const db = mysql.createConnection({
--- store-app-vg/src/components/App.jsx
+++ store-app-vg/src/components/App.jsx
@@ -1,11 +1,22 @@
-import Header from "./Header/Header"
-
+import Header from "./Header/Header";
+import { useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState(null);
+  const fetchProds = () => {
+    fetch("http://localhost:3000/products")
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data));
+  };
+
+  console.log(products);
 
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <button onClick={() => fetchProds()}>get that data!</button>
+    </>
+  );
 }
 
-export default App
+export default App;
